756e6b66a13aa43866383b90abeefe8ab58d340a
Valeriya Griffiths <v.griffiths@cantabrisk.com>
2021-11-03 18:58:49 +0000

    delete admin

--- brainlabs_test/urls.py
+++ brainlabs_test/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from country_quiz.views import quiz
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', quiz, name='quiz')
 ]
