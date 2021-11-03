95ec46580c1e124979c46aa0a0063f197df8fd70
Valeriya Griffiths <v.griffiths@cantabrisk.com>
2021-11-03 18:21:55 +0000

    pass data to answer template, after post request

--- country_quiz/views.py
+++ country_quiz/views.py
@@ -6,9 +6,23 @@ from .services import get_country_data_from_external_api
 def quiz(request):
     """Generates quiz question and provides form to submit answer."""
 
-    data = get_country_data_from_external_api()
-    country = data[0]['name']
-    form = QuizForm()
+    if request.method == 'POST':
+        form = QuizForm(request.POST)
+        if form.is_valid():
+            return render(request, 'country_quiz/check_answer.html', {'answer': form.data['answer'],
+                                                                      'capital': request.session['capital'],
+                                                                      'country': request.session['country']
+                                                                      })
+        else:
+            pass #TODO
+    else:
+        data = get_country_data_from_external_api() #TODO calls api every time and only takes first.
+        country = data[0]['name']
+        capital = data[0]['capital']
+
+        request.session['country'] = country
+        request.session['capital'] = capital
+        form = QuizForm()
 
     return render(request, 'country_quiz/quiz_question.html', {'country': country,
                                                                'form': form})
